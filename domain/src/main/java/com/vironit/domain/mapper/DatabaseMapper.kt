5911aa616b88b216bd96152587c56b7237f7069b
e.krasnovskaya <ElizavetaKrasnovskaya@gmail.com>
2022-02-02 14:40:26 +0300

    git init

--- domain/src/main/java/com/vironit/domain/mapper/DatabaseMapper.kt
+++
@@ -1,6 +0,0 @@
-package com.vironit.domain.mapper
-
-interface DatabaseMapper<E, M> {
-    fun fromEntity(from: E): M
-    fun toEntity(from: M): E
-}
--- domain/src/main/java/com/vironit/domain/mapper/RetrofitMapper.kt
+++
@@ -1,6 +0,0 @@
-package com.vironit.domain.mapper
-
-interface RetrofitMapper<E, M> {
-    fun fromUnsplash(from: E): M
-    fun toUnsplash(from: M): E
-}
